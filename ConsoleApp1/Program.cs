40b636d3c5e2521fc19867a8cf861db8558a062c
Sashakrm <sasakramch@gmail.com>
2024-11-04 20:54:58 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("выберите вариант:");
+            Console.WriteLine("1. Периметр");
+            Console.WriteLine("2. Площадь");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: perimetr(); break;
+                case 2: area(); break;
+                default: Console.WriteLine("Неправильный выбор."); break;
+            }
+        }
+
+        static void perimetr()
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            double sum = 2 * (a + b);
+            Console.WriteLine($"Периметр: {sum}");
+        }
+
+        static void area()
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            double area = a * b;
+            Console.WriteLine($"Площадь: {area}");
+        }
+    }
+}
+
